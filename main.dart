b0fd1714af986456f7cb80dd555cf87b6c46ced3
JuergenKuck <drj.kuck@googlemail.com>
2025-02-18 11:50:29 +0100

    B. Prints

--- main.dart
+++ main.dart
@@ -1,15 +1,33 @@
+// ignore_for_file: unused_local_variable
+
 void main() {
+  // Aufgabe 1
+  // A.
   String gdStr = "Guten";
   String dayStr = "Tag";
-  String worldStr = "World";
-  int d = 20;
+  String worldStr = "Welt";
+  int d1 = 20;
   String timeStr = "Zeit";
   int seven = 7;
   int twelve = 12;
-  String d = "!";
+  String d2 = "!";
   int a = 24;
   String c = "Schöne";
   String morning = "Morgen";
   String s = "Heute";
   String spa = " ";
+
+// B.
+  String goodDay = "$gdStr$spa$dayStr";
+  print(goodDay);
+
+  print("$goodDay$spa$worldStr$d2");
+
+  print("${seven + a}");
+
+  print("$c$spa$timeStr$d2");
+
+  print("$s$spa$morning");
+
+  print("${seven * a}");
 }
